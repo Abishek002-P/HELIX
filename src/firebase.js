7fa40157463a913e7cd3351ed2b61fe951b85af7
Abishek002-P <abishekprabakaran2003@gmail.com>
2022-01-21 15:04:23 +0530

    Created basic structure

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDqo6NkU33IneSNUb7pvXsxCzwl2Kdd67U",
+  authDomain: "helix-xd.firebaseapp.com",
+  projectId: "helix-xd",
+  storageBucket: "helix-xd.appspot.com",
+  messagingSenderId: "1080551245890",
+  appId: "1:1080551245890:web:760f1e6e8bcff75951c5db",
+  measurementId: "G-VF61RZHW3P"
+};
+
+// Initialize Firebase
+firebaseConfig.initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
