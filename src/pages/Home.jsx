76c52647ee802da08448b1fa9a475d8b807f2079
Varshini <varshinibalakumar@gmail.com>
2022-01-21 22:36:44 +0530

    initial commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,3 +10,18 @@ export default function Home() {
   const [loader, setLoader] = useState(true);
   const [videoItems, setVideoItems] = useState([]);
   const [videoId, setVideoId] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails%2Cstatus&playlistId=PLLCdGWbcw9uwhUPaCmtQlHlMKyE6R7a1P&key=${REACT_APP_YOUTUBE_API_KEY}`
+      )
+      .then((res) => {
+        const videoitems = res.data.items;
+        setVideoItems(videoitems);
+        setLoader(false);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+
