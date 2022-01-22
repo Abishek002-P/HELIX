6a864d7dd5dc10f412cceb0971f3e14937e435b3
Abishek002-P <abishekprabakaran2003@gmail.com>
2022-01-22 16:00:05 +0530

    Removed live page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import {
 import { useEffect } from "react";
 import LandingPage from "./pages/LandingPage";
 import Home from "./pages/Home";
-import Live from "./pages/Live";
 import Error404 from "./pages/Error404";
 import PrivateRoute from "./PrivateRoute";
 import { checkTheme } from "./pages/theme";
@@ -21,7 +20,7 @@ export default function App() {
       <Switch>
         <Route exact path="/" component={LandingPage} />
         <PrivateRoute exact path="/home" component={Home} />
-        <PrivateRoute exact path="/live" component={Live} />
+        {/* // <PrivateRoute exact path="/live" component={Live} /> */}
         <Route exact path="/404" component={Error404} />
         <Redirect to="/404" />
       </Switch>
