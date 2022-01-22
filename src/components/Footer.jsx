8d66175f52ed03a1a532991a77726234dcb47b7f
Abishek002-P <abishekprabakaran2003@gmail.com>
2022-01-22 14:04:20 +0530

    Footer alignment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,9 @@
             <div className="bg-secondary" style={{ height: "1px" }}></div>
           </div>
           <div className="text-center text-muted d-flex flex-column flex-md-row  justify-content-between align-items-center my-3 mx-md-3  ml-md-4 ml-lg-0">
-            © {currentYear} · Helix · All rights reserved
+           <p className="mb-0">
+           © {currentYear} · Helix · All rights reserved
+           </p>
           </div>
         </section>
       </>
