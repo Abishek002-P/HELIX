59bc53fb4feb492a52b765b0c9a6ad1dda61d17c
Abishek002-P <abishekprabakaran2003@gmail.com>
2022-01-22 14:00:32 +0530

    Hid the keys&api

--- src/firebase.js
+++ src/firebase.js
@@ -7,21 +7,28 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-// import firebase from "firebase/app";
+import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
+let { REACT_APP_API_KEY } = process.env;
+let { REACT_APP_AUTH_DOMAIN } = process.env;
+let { REACT_APP_PROJECT_ID } = process.env;
+let { REACT_APP_STORAGE_BUCKET } = process.env;
+let { REACT_APP_MESSAGING_SENDER_ID } = process.env;
+let { REACT_APP_APP_ID } = process.env;
+let { REACT_APP_MEASUREMENTID } = process.env;
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDqo6NkU33IneSNUb7pvXsxCzwl2Kdd67U",
-  authDomain: "helix-xd.firebaseapp.com",
-  projectId: "helix-xd",
-  storageBucket: "helix-xd.appspot.com",
-  messagingSenderId: "1080551245890",
-  appId: "1:1080551245890:web:760f1e6e8bcff75951c5db",
-  measurementId: "G-VF61RZHW3P"
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+  appId: REACT_APP_APP_ID,
+  measurementId: REACT_APP_MEASUREMENTID
 };
 
 // Initialize Firebase
-firebaseConfig.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
