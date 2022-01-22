4bf5fb58f329bed93c402572fa924f5a20a4d345
Abishek002-P <abishekprabakaran2003@gmail.com>
2022-01-22 14:00:18 +0530

    Added privateroute and route for all pages

--- src/App.js
+++ src/App.js
@@ -4,18 +4,27 @@ import {
   Route,
   Redirect,
 } from "react-router-dom";
-
+import { useEffect } from "react";
 import LandingPage from "./pages/LandingPage";
+import Home from "./pages/Home";
+import Live from "./pages/Live";
+import Error404 from "./pages/Error404";
+import PrivateRoute from "./PrivateRoute";
+import { checkTheme } from "./pages/theme";
 
-function App() {
+export default function App() {
+  useEffect(() => {
+    checkTheme();
+  }, []);
   return (
     <Router>
       <Switch>
-      <Route exact path="/" component={LandingPage} />
-      <Redirect to="/404" />
+        <Route exact path="/" component={LandingPage} />
+        <PrivateRoute exact path="/home" component={Home} />
+        <PrivateRoute exact path="/live" component={Live} />
+        <Route exact path="/404" component={Error404} />
+        <Redirect to="/404" />
       </Switch>
     </Router>
   );
 }
-
-export default App;
